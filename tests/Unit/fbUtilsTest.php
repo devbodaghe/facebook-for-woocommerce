932cd832b09613266e68d68f8c29a25b02497a73
David Evbodaghe <davidevbodaghe@meta.com>
2025-04-12 16:07:25 +0100

    [BE] add tests for util methods in fbUtils

--- tests/Unit/fbUtilsTest.php
+++ tests/Unit/fbUtilsTest.php
@@ -37,4 +37,96 @@ class fbUtilsTest extends WP_UnitTestCase {
         $actualOutput = WC_Facebookcommerce_Utils::clean_string($string, true);
         $this->assertEquals($expectedOutput, $actualOutput);
     }
+
+    public function testNormalizeProductDataDefaultCondition() {
+        $data = [
+            'title' => 'Test Product'
+        ];
+        
+        $normalized = WC_Facebookcommerce_Utils::normalize_product_data_for_items_batch($data);
+        
+        $this->assertEquals('new', $normalized['condition']);
+    }
+
+    public function testNormalizeProductDataKeepsValidCondition() {
+        $validConditions = ['refurbished', 'used', 'new'];
+        
+        foreach ($validConditions as $condition) {
+            $data = [
+                'title' => 'Test Product',
+                'condition' => $condition
+            ];
+            
+            $normalized = WC_Facebookcommerce_Utils::normalize_product_data_for_items_batch($data);
+            
+            $this->assertEquals($condition, $normalized['condition']);
+        }
+    }
+
+    public function testNormalizeProductDataInvalidCondition() {
+        $data = [
+            'title' => 'Test Product',
+            'condition' => 'invalid_condition'
+        ];
+        
+        $normalized = WC_Facebookcommerce_Utils::normalize_product_data_for_items_batch($data);
+        
+        $this->assertEquals('new', $normalized['condition']);
+    }
+
+    public function testNormalizeProductDataCustomAttributes() {
+        $data = [
+            'title' => 'Test Product',
+            'custom_data' => [
+                'material' => 'cotton',
+                'style' => 'casual'
+            ]
+        ];
+        
+        $normalized = WC_Facebookcommerce_Utils::normalize_product_data_for_items_batch($data);
+        
+        $this->assertArrayNotHasKey('custom_data', $normalized);
+        $this->assertArrayHasKey('additional_variant_attribute', $normalized);
+        $this->assertEquals('material:cotton,style:casual', $normalized['additional_variant_attribute']);
+    }
+
+    public function testNormalizeProductDataAttributesWithCommas() {
+        $data = [
+            'title' => 'Test Product',
+            'custom_data' => [
+                'material' => 'cotton, polyester',
+                'style' => 'casual, formal'
+            ]
+        ];
+        
+        $normalized = WC_Facebookcommerce_Utils::normalize_product_data_for_items_batch($data);
+        
+        $this->assertEquals('material:cotton polyester,style:casual formal', $normalized['additional_variant_attribute']);
+    }
+
+    public function testNormalizeProductDataAttributesWithColons() {
+        $data = [
+            'title' => 'Test Product',
+            'custom_data' => [
+                'material:type' => 'cotton:blend',
+                'style:category' => 'casual:wear'
+            ]
+        ];
+        
+        $normalized = WC_Facebookcommerce_Utils::normalize_product_data_for_items_batch($data);
+        
+        $this->assertEquals('material type:cotton blend,style category:casual wear', $normalized['additional_variant_attribute']);
+    }
+
+    public function testNormalizeProductDataEmptyCustomData() {
+        $data = [
+            'title' => 'Test Product',
+            'custom_data' => []
+        ];
+        
+        $normalized = WC_Facebookcommerce_Utils::normalize_product_data_for_items_batch($data);
+        
+        $this->assertArrayNotHasKey('custom_data', $normalized);
+        $this->assertArrayNotHasKey('additional_variant_attribute', $normalized);
+    }
 }
